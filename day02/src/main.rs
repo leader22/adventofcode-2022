b15dcef4a7bacee3b764694cc14995fe8b1f9cd1
Yuji Sugiura <y.sugiura.0316@gmail.com>
2022-12-02 15:15:39 +0900

    Day 02

---
+++ day02/src/main.rs
@@ -0,0 +1,50 @@
+// const INPUTS: &str = r#"
+// A Y
+// B X
+// C Z
+// "#;
+const INPUTS: &str = include_str!("./input.txt");
+
+fn main() {
+    let (lost, draw, win) = (0, 3, 6);
+    let (rock, paper, scissors) = (1, 2, 3);
+
+    let guide_hands = INPUTS
+        .trim()
+        .lines()
+        .map(|line| line.split_once(' ').unwrap());
+
+    let part1 = guide_hands
+        .clone()
+        .map(|hand| match hand {
+            ("A", "X") => rock + draw,
+            ("A", "Y") => paper + win,
+            ("A", "Z") => scissors + lost,
+            ("B", "X") => rock + lost,
+            ("B", "Y") => paper + draw,
+            ("B", "Z") => scissors + win,
+            ("C", "X") => rock + win,
+            ("C", "Y") => paper + lost,
+            ("C", "Z") => scissors + draw,
+            _ => unreachable!(),
+        })
+        // .map(|x| dbg!(x))
+        .sum::<i32>();
+    println!("part1: {}", part1);
+
+    let part2 = guide_hands
+        .map(|hands| match hands {
+            ("A", "X") => lost + scissors,
+            ("A", "Y") => draw + rock,
+            ("A", "Z") => win + paper,
+            ("B", "X") => lost + rock,
+            ("B", "Y") => draw + paper,
+            ("B", "Z") => win + scissors,
+            ("C", "X") => lost + paper,
+            ("C", "Y") => draw + scissors,
+            ("C", "Z") => win + rock,
+            _ => unreachable!(),
+        })
+        .sum::<i32>();
+    println!("part2: {}", part2);
+}
