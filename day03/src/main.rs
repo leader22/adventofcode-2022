d5a88e950c042622b4cb7346163f9ff86ab3219d
Yuji Sugiura <y.sugiura.0316@gmail.com>
2022-12-06 09:59:11 +0900

    Update day 03

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -20,6 +20,16 @@ fn main() {
 
     let part1 = common_items.map(char_to_priority).sum::<usize>();
     println!("{}", part1);
+
+    let part2 = INPUTS
+        .trim()
+        .lines()
+        .collect::<Vec<_>>()
+        .chunks(3)
+        .filter_map(|group| find_common_chars2(group[0], group[1], group[2]))
+        .map(char_to_priority)
+        .sum::<usize>();
+    println!("{}", part2);
 }
 
 // c as usize
@@ -48,6 +58,49 @@ fn find_common_chars(s1: &str, s2: &str) -> Option<char> {
             }
         }
     }
+    None
+}
+
+fn find_common_chars2(s1: &str, s2: &str, s3: &str) -> Option<char> {
+    let mut lower1 = [0; 26];
+    let mut upper1 = [0; 26];
+    for c in s1.chars() {
+        if c.is_uppercase() {
+            lower1[c as usize - 65] = 1;
+        } else {
+            upper1[c as usize - 97] = 1;
+        }
+    }
+
+    let mut lower2 = [0; 26];
+    let mut upper2 = [0; 26];
+    for c in s2.chars() {
+        if c.is_uppercase() {
+            lower2[c as usize - 65] = 1;
+        } else {
+            upper2[c as usize - 97] = 1;
+        }
+    }
+
+    let mut lower3 = [0; 26];
+    let mut upper3 = [0; 26];
+    for c in s3.chars() {
+        if c.is_uppercase() {
+            lower3[c as usize - 65] = 1;
+        } else {
+            upper3[c as usize - 97] = 1;
+        }
+    }
+
+    for i in 0..=25 {
+        if lower1[i] + lower2[i] + lower3[i] == 3 {
+            return Some((i as u8 + 65) as char);
+        }
+        if upper1[i] + upper2[i] + upper3[i] == 3 {
+            return Some((i as u8 + 97) as char);
+        }
+    }
+
     None
 }
 
